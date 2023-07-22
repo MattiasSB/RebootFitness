2b62f814e1b9f6ef762296d634efa202aa118d95
dajeongpaak <101000717+dajeongpaak@users.noreply.github.com>
2023-07-22 09:28:17 -0700

    Initial IDE setup
    - Created React App
    - Separated frontend folder in case we might have backend in the future
    - Install and configure firebase

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      WELCOME TO REBOOT MATTI!
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/firebase.config.js
@@ -0,0 +1,31 @@
+
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBTS6bDyGDz4MoeXUDUiF-VDURGLBjNWEM",
+
+  authDomain: "reboot-fitness-db8a4.firebaseapp.com",
+
+  projectId: "reboot-fitness-db8a4",
+
+  storageBucket: "reboot-fitness-db8a4.appspot.com",
+
+  messagingSenderId: "1068298833323",
+
+  appId: "1:1068298833323:web:d1911d1b1379f5f076d6ee"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
