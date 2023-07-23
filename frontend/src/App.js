14452ac4af75ffa996b4e7f9b2e14f2a1abb8b11
dajeongpaak <101000717+dajeongpaak@users.noreply.github.com>
2023-07-23 00:10:01 -0700

    Setup IDE
    - Initial setup of SASS
    - Set up React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import './App.css';
+import './App.scss';
+import Home from './pages/home'
+import SignIn from './pages/signIn'
+import SignUp from './pages/signUp'
 
 function App() {
   return (
-    <div className="App">
-      WELCOME TO REBOOT MATTI!
-    </div>
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+
+import styles from './Home.module.scss'
+
+export default function Home() {
+  return (
+    <>
+        <h1 className={styles.title}>
+        Welcome back to Reboot Matti!
+        </h1>
+    </>
+  )
+}
