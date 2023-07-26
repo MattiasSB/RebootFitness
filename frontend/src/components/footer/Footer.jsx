46f3b7d56c8306279ae9c9374d0adc3a948caed0
mattiasbellan <m.bellan500@gmail.com>
2023-07-25 23:29:57 -0700

    added changes from main as well as removed test

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -2,8 +2,8 @@ import styles from './Footer.module.scss'
 
 export default function Footer() {
   return (
-    <div className={styles.djvn}>
-      <h1>Hello</h1>
+    <div>
+      footer
     </div>
   )
 }
