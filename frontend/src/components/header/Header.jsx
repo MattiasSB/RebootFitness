ad04290233abbc7210ecab74d8a264d71b56dd94
mattiasbellan <m.bellan500@gmail.com>
2023-07-29 16:16:37 -0700

    Fixed header scroll

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -14,6 +14,8 @@ export default function Header() {
 
   let prevScrollpos = window.scrollY;
 
+  
+
   const handleScroll = () => {
       const currentScrollPos = window.scrollY;
       if (prevScrollpos > currentScrollPos - 1) {
@@ -40,12 +42,36 @@ export default function Header() {
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []); 
+  }, ); 
 
 // Function to toggle the menu state
 const handleMenuToggle = () => {
-  setMenuOpen((prevMenuState) => !prevMenuState);
+  setMenuOpen((prevMenuState) => {
+    if (!prevMenuState) {
+      // Add the class to disable scrolling when the menu is opened
+      document.body.style.overflow = "hidden";
+    } else {
+      // Remove the class to enable scrolling when the menu is closed
+      document.body.style.overflow = "initial"
+    }
+    return !prevMenuState;
+  });
+
 };
+
+useEffect(() => {
+  const handleResize = () => {
+    setMenuOpen(false);
+    // Remove the class to enable scrolling when the menu is closed
+    document.body.style.overflow = "initial"
+  };
+
+  window.addEventListener('resize', handleResize);
+
+  return () => {
+    window.removeEventListener('resize', handleResize);
+  };
+}, []);
 
   return (
     <header ref={navRef}>
