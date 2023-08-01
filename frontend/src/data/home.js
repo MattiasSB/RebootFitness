ab010a0160f2cfd32741fa4b830bbdc72ad52661
dajeongpaak <101000717+dajeongpaak@users.noreply.github.com>
2023-07-31 23:13:20 -0700

    Complete home page
    - made it responsive for mobile, tablet and desktop

--- frontend/src/data/home.js
+++ frontend/src/data/home.js
@@ -4,6 +4,29 @@ export const home = [
         heading: "welcome to reboot",
         desc: "Welcome to Reboot, where fitness is more than just a workout, it's a journey of self-discovery and empowerment. Our passion for health and wellness drives us to provide a holistic fitness experience that goes beyond physical transformation. Here, we believe that every individual has the potential to achieve greatness, and our mission is to guide and support you in unlocking that potential.",
         btnText: "learn more",
-        src: ""
+        navigateTo: "/about",
+        src: require('../assets/images/welcome.jpg'),
+        src_m: require('../assets/images/welcome-mobile.jpg'),
+    },
+    {
+        subheading: "classes",
+        heading: "discover your fitness adventure",
+        desc: "Ready to elevate your fitness journey? Book a class today at Reboot and experience the thrill of our dynamic group fitness sessions. Whether you're craving an intense cycle workout or seeking zen through yoga, our diverse classes cater to all interests and fitness levels. Don't wait - reserve your spot now!",
+        btnText: "view classes",
+        navigateTo: "/classes",
+        src: require('../assets/images/discover.jpg'),
+        src_m: require('../assets/images/discover-mobile.jpg'),
+    },
+    {
+        subheading: "get in touch",
+        heading: "reach out to us today",
+        desc: "Whether it's a question, a concern, or just a friendly hello, we'd love to hear from you. Our communication channels are always open, and our team is ready to assist you with a smile. So, feel free to get in touch through phone or by visiting any of our facilities. ",
+        contact: "+1 (234) - 567 - 8910",
+        btnText: "contact us",
+        navigateTo: "tel:+12345678910",
+        src: require('../assets/images/reach-out.jpg'),
+        src_2: require('../assets/images/reach-out-2.jpg'),
+        src_m: require('../assets/images/reach-out-mobile.jpg'),
+        src_m_2: require('../assets/images/reach-out-mobile-2.jpg'),
     }
 ]
